58d698707cbe5cbb510c7d2f789a784f0f65201c
Jeremy Kiel <jeremykiel11@gmail.com>
2021-09-24 11:46:50 +1200

    Touched up UI sprites and marked up in Unity
    Removed transparent gap around UI sprites, renamed and a few moved around.
    Added a few event calls in GameUI.cs

--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -17,6 +17,12 @@ public class GameUI : MonoBehaviour {
 
 	public Text TimeDisplay;
 
+	[Space]
+	[Header("HUD Elements")]
+
+	public Text Player1DNAText;
+	public Text Player2DNAText;
+
 
 	//----------------------------------------------------------------------------------------------------------------------------------<
 
@@ -56,4 +62,51 @@ public class GameUI : MonoBehaviour {
 	}
 
 
+	//----------------------------------------------------------------------------------------------------------------------------------<
+
+
+	/// <summary>
+	/// Displays DNA Count in player HUD for specified player. (0 = player one), (1 = player two/AI).
+	/// </summary>
+	/// <param name="count"></param>
+	/// <param name="player">(0 = player one), (1 = player two/AI)</param>
+	public void UpdateDNA(int count, int player) => (player == 0 ? Player1DNAText : Player2DNAText).text = "DNA: " + count.ToString();
+
+
+	//----------------------------------------------------------------------------------------------------------------------------------<
+
+
+	#region Input Events
+
+
+	//----------------------------------------------------------------------------------------------------------------------------------<
+
+
+	/// <summary>
+	/// Is called by Path (Surface/Tunnel) button click event.
+	/// </summary>
+	/// <param name="path">(0 = surface/top), (1 = tunnel/bottom)</param>
+	/// <param name="player">(0 = player one), (1 = player two/AI)</param>
+	public void OnPathSelectClick(int path, int player) {
+		//Hook into base script
+	}
+
+
+	//----------------------------------------------------------------------------------------------------------------------------------<
+
+
+	/// <summary>
+	/// Is called by Unit (Soldier/Spitter/Tank) button click event.
+	/// </summary>
+	/// <param name="unit">(0 = soldier), (1 = spitter), (2 = tank)</param>
+	/// <param name="player">(0 = player one), (1 = player two/AI)</param>
+	public void OnUnitSpawnClick(int unit, int player) {
+		//Hook into base script
+	}
+
+
+	//----------------------------------------------------------------------------------------------------------------------------------<
+
+
+	#endregion
 }
