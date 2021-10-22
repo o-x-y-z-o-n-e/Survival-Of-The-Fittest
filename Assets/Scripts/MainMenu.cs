73fbcafaeeeac3c6f67bac7cff1d73b74f1a594b
Jeremy Kiel <jeremykiel11@gmail.com>
2021-10-23 12:36:46 +1300

    Added Difficulty Select in Start Menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -13,8 +13,11 @@ public class MainMenu : MonoBehaviour {
 	const string LEVEL_SCENE_NAME = "Level-1";
 
 
-	public Dropdown Player1Dropdown;
-	public Dropdown Player2Dropdown;
+	public Dropdown Player1Mode;
+	public Dropdown Player2Mode;
+
+	public Dropdown Player1Difficulty;
+	public Dropdown Player2Difficulty;
 
 
 	//----------------------------------------------------------------------------------------------------------------------------------<
@@ -24,10 +27,19 @@ public class MainMenu : MonoBehaviour {
 	/// Will load and start a new level. At the moment there is only one level. So no level select is implemented yet. Called by the 'Play' button on the main menu.
 	/// </summary>
 	public void OnPlayClick() {
-		Options.Player1Mode = (PlayerMode)Player1Dropdown.value;
-		Options.Player2Mode = (PlayerMode)Player2Dropdown.value;
+		Options.Player1Mode = (PlayerMode)Player1Mode.value;
+		Options.Player2Mode = (PlayerMode)Player2Mode.value;
 
 		SceneManager.LoadScene(LEVEL_SCENE_NAME);
+	}
+
+
+	//----------------------------------------------------------------------------------------------------------------------------------<
+
+
+	public void OnPlayerModeUpdate() {
+		Player1Difficulty.gameObject.SetActive((PlayerMode)Player1Mode.value == PlayerMode.AI);
+		Player2Difficulty.gameObject.SetActive((PlayerMode)Player2Mode.value == PlayerMode.AI);
 	}
 
 
