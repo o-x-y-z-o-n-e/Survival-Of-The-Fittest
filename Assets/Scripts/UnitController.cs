6796992ece74a0ae44c00a33011f665813a801ff
Brayden Cooke <87751532+Cookiezznz@users.noreply.github.com>
2021-10-24 19:25:55 +1300

    Fix/DamageCalc
    Fixed damage calculation
    Changed evolution numbers

--- Assets/Scripts/UnitController.cs
+++ Assets/Scripts/UnitController.cs
@@ -27,12 +27,12 @@ public class UnitController : MonoBehaviour, Damageable {
 	const float			FRIENDLY_OVERLAP		= 0.0f;
 	const float			ALLY_CHECK_DIST			= 0.1f;
 
-	const float BLOODLUST_THRESHOLD = 0.50f; //What percent of total health does a Unit have to reach below for bloodlust to activate.
-	const float BLOODLUST_DAMAGE_MULTIPLIER = 5f;
-	const float BLOODLUST_MOVE_MULTIPLIER = 1f;
+	const float BLOODLUST_THRESHOLD = 0.40f; //What percent of total health does a Unit have to reach below for bloodlust to activate.
+	const float BLOODLUST_DAMAGE_MULTIPLIER = 1.3f;
+	const float BLOODLUST_MOVE_MULTIPLIER = 1.3f;
 
 
-	public const float CRITICAL_DAMAGE_MULTIPLIER = 3f;
+	public const float CRITICAL_DAMAGE_MULTIPLIER = 1.5f;
 
 
 	const float ATTACK_LUNGE_TIME = 0.25f;
@@ -372,18 +372,14 @@ public class UnitController : MonoBehaviour, Damageable {
 			//with rand(-1, 1) too many values where close to 0. I changed the chances gravitate towards +- 1 by having a linear range of [-2, 2] and clamping to [-1, 1]
 			attackLungeHeightMulitplier = Mathf.Clamp(UnityEngine.Random.Range(-2f, 2f), -1f, 1f);
 		}
-
 		if (TryCritical()) {
 			d = (int)(d * CRITICAL_DAMAGE_MULTIPLIER);
-
 			punchIconCounter = PUNCH_ICON_TIME;
 			punchIcon.transform.localPosition = Vector3.zero;
 			punchIcon.transform.gameObject.SetActive(true);
-
 			StartCoroutine(CritHitVisual());
 		}
-
-		if (nextEnemy.TakeDamage(damage, unitOwner)) {
+		if (nextEnemy.TakeDamage(d, unitOwner)) {
 			unitOwner.ChangeDNA(modifiers.ExtraDNAHarvest);
 		}
 
